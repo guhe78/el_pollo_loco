c82f22106b5f10e827daf756efa93cebcd1cf454
Guenter Heldt <gh1978@hotmail.de>
2026-05-08 08:59:24 +0200

    refactor: added section class

--- level/level1.js
+++ level/level1.js
@@ -1,33 +1,42 @@
 const enemiesCount = 10;
 const firstEnemyX = 500;
 const lastEnemyX = 2300;
-const level1 = new Level(
-  createEnemies(10),
-  new Endboss("img/Enemy/3 Final Enemy/2.floating/1.png", 3000, 100),
-  [
-    new Barrier("img/Background/Barrier/1.png", 0, 0, 350, 300),
-    new Barrier("img/Background/Barrier/2.png", 200, 0, 350, 300),
-    new Barrier("img/Background/Barrier/3.png", 400, 0, 350, 300),
-  ],
-  [
-    new BackgroundObject("img/Background/Layers/3.Fondo 1/D.png", 0),
-    new BackgroundObject("img/Background/Layers/4.Fondo 2/D.png", 0),
-    new BackgroundObject("img/Background/Layers/2. Floor/D.png", 0),
-    new BackgroundObject("img/Background/Layers/3.Fondo 1/D.png", 1703),
-    new BackgroundObject("img/Background/Layers/4.Fondo 2/D.png", 1703),
-    new BackgroundObject("img/Background/Layers/2. Floor/D.png", 1703),
-  ],
-  [
-    new BackgroundWater("img/Background/Layers/5. Water/D.png", 0),
-    new BackgroundWater("img/Background/Layers/5. Water/D.png", 1703),
-  ],
-  createCollectibles(10),
-);
+const level1 = new Level([
+  new Section(
+    0,
+    createBackgrounds(6000),
+    createEnemies(10),
+    createCollectibles(10),
+  ),
+  6000,
+]);
+
+function createBackgrounds(levelLength) {
+  let backgrounds = [];
+  const width = 1705;
+
+  for (let x = 0; x < levelLength; x += width) {
+    backgrounds.push(
+      new BackgroundWater("img/Background/Layers/5. Water/L.png", x),
+    );
+    backgrounds.push(
+      new BackgroundObject("img/Background/Layers/3.Fondo 1/D.png", x),
+    );
+    backgrounds.push(
+      new BackgroundObject("img/Background/Layers/4.Fondo 2/D.png", x),
+    );
+    backgrounds.push(
+      new BackgroundObject("img/Background/Layers/2. Floor/L.png", x),
+    );
+  }
+
+  return backgrounds;
+}
 
 function createEnemies(count) {
   let enemies = [];
-  let spacing = (lastEnemyX - firstEnemyX) / count - 1;
-  for (i = 0; i < count; i++) {
+  let spacing = (lastEnemyX - firstEnemyX) / (count - 1);
+  for (let i = 0; i < count; i++) {
     const position_x = firstEnemyX + i * spacing + randomNumber(120) - 60;
     const position_y = randomNumber(420);
     if (randomNumber(2) === 0) {
@@ -42,7 +51,7 @@ function createEnemies(count) {
 function createCollectibles(count) {
   let collectibles = [];
   let half = count / 2;
-  for (i = 0; i < count; i++) {
+  for (let i = 0; i < count; i++) {
     const position_x = randomNumber(2500);
     const position_y = randomNumber(400);
     if (i < half) {
