8aab389398f2b4d54a915d30a90eba17108391a7
Guenter Heldt <gh1978@hotmail.de>
2026-05-28 17:55:45 +0200

    deleted unused files

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
+/**
+ * Initializes the game bootstrap flow.
+ */
 function init() {
   loadCanvas();
 }
 
+/**
+ * Resumes gameplay from pause state.
+ */
 function continueGame() {
   world.togglePause();
 }
 
+/**
+ * Restarts the game by re-running initialization.
+ */
 function restartGame() {
   init();
 }
 
+/**
+ * Leaves the current run and returns to start menu.
+ */
 function quitGame() {
   world.startMenu();
 }
 
+/**
+ * Opens the start menu.
+ */
 function startMenu() {
   world.startMenu();
 }
